74b8fbcbfe46b317aca9e22ef147fc2778011e4f
khaitrank366 <khaitran.k366@gmail.com>
2025-07-04 09:08:41 +0700

    update

---
+++ Day01_HTML/script.js
@@ -0,0 +1,11 @@
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header-placeholder').innerHTML = data);
+
+fetch('navbar.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('nav-placeholder').innerHTML = data);
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer-placeholder').innerHTML = data);
